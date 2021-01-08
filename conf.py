5ad9ffd490510d070efc9d45529346b92bdb63c4
K.Harideep <51321740+harideep8@users.noreply.github.com>
2021-01-08 12:27:26 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+extensions = ['recommonmark']
 
 # -- Options for HTML output -------------------------------------------------
 
