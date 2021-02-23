1367706ced8494ee866fbd6db9b2a0c385b56fc6
vijaykrishna2601 <67304999+vijaykrishna2601@users.noreply.github.com>
2021-02-23 16:44:25 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'Git & Github'
+copyright = '2021, Vijay Krishna'
+author = 'Vijay Krishna'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
